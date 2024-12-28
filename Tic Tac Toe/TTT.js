5a61bf7e4e2f7a3e47d5a08368ddcc48230bf6ab
aman_sama <amanrai2002acr@gmail.com>
2024-12-28 20:17:14 +0545

    updated TTT

--- Tic Tac Toe/TTT.js
+++ Tic Tac Toe/TTT.js
@@ -50,3 +50,12 @@ const checkWinner = () => {
     }
   }
 };
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.scr = "moon.png";
+  } else {
+    icon.src = "sun.png";
+  }
+};
